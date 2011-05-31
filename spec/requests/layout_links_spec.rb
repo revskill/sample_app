832d0ed1fb6ff9fb1487ee0ac3ff45fac961e16d
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-05-31 14:51:47 +0700

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -7,18 +7,23 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Home")
   end
 
-  it "should have a Contact page at '/pages/contact'" do
+  it "should have a Contact page at '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
 
-  it "should have an About page at '/pages/about'" do
+  it "should have an About page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
 
-  it "should have a Help page at '/pages/help'" do
+  it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
   end
 end
