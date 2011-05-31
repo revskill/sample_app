592668e6f65732e02b06344913c46cb40e5f63d3
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-05-31 14:21:28 +0700

    Second commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   def home
 	@title = "Home"
   end
-
+  def about
+	@title = "About"
+  end
   def contact
 	@title = "Contact"
   end
-
+  def help
+    @title = "Help"
+  end
 end
